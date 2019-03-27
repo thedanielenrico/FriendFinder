a2219f9178225ea1e42728c7c83db1233ca03823
thedanielenrico <thedanielenrico@gmail.com>
2019-03-27 06:44:57 -0700

    Build out questions for survey page

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,30 @@
 var friendsList = [
     {
         name: "Dan",
-        surveryScores: [1,2,3,4,5,6]
-    }
+        image: "https://goo.gl/images/jj9Z8c",
+        surveryScores: [1,2,3,4,5,2]
+    },
+    {
+        name: "Janice",
+        image: "https://goo.gl/images/jj9Z8c",
+        surveryScores: [3,4,5,2,1,5]
+    },
+    {
+        name: "Billy",
+        image: "https://goo.gl/images/jj9Z8c",
+        surveryScores: [4,2,3,3,2,1]
+    },
+    {
+        name: "Shannon",
+        image: "https://goo.gl/images/jj9Z8c",
+        surveryScores: [4,4,3,5,5,5]
+    },
+    {
+        name: "Riley",
+        image: "https://goo.gl/images/jj9Z8c",
+        surveryScores: [3,2,1,3,4,5]
+    },
+
 ]
 
 module.exports = friendsList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,3 +10,16 @@ module.exports = apiRoutes = function (router) {
         res.sendFile(path.join(__dirname, "../data/friends.js"));
     });
 }
+$("#submit").on('click', function (event) {
+    event.preventDefault();
+    var newUser = {
+        name: $("#userName").val().trim(),
+        lastName: $("#userLastName").val().trim()
+    }
+    console.log(newUser)
+    $.post("api/friends", newUser)
+        .then(function (data) {
+            console.log(data)
+        })
+
+})
